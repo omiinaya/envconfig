fdb207426687f348a25ac27bc24457dec8222374
Omar Minaya <omiinaya@gmail.com>
2022-10-09 00:45:24 -0400

    added vue bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -13,40 +13,6 @@ export default {
   components: {
     HelloWorld,
   },
-  methods: {
-    async print(a) {
-      window.webContents.send('LOG_REQUEST', a);
-    },
-    async getNodeList() {
-      const url = 'https://nodejs.org/dist/';
-      const req = await fetch(url);
-      const string = await req.text();
-      const lines = string.split('\n');
-      const versions = lines.filter((line) => line.includes('<a href="v'));
-      const parsed = [];
-      versions.forEach((line) => parsed.push(line.split('href="v').pop().split('/">')[0]));
-      parsed.sort((a, b) => {
-        const num1 = Number(
-          a
-            .split('.')
-            .map((num) => `000${num}`.slice(-3))
-            .join(''),
-        );
-        const num2 = Number(
-          b
-            .split('.')
-            .map((num) => `000${num}`.slice(-3))
-            .join(''),
-        );
-        return num1 - num2;
-      });
-      const results = parsed.reverse().slice(0, 100);
-      this.print(results);
-    },
-  },
-  created() {
-    this.getNodeList();
-  },
 };
 </script>
 
